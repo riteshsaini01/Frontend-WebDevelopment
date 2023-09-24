6bdf5885830a3c63ccebd07e94cc8c763e914ac4
riteshsaini01 <b210885@skit.ac.in>
2023-09-24 18:58:39 +0530

    datatypes

---
+++ JavaScript/01_Variables.js
@@ -0,0 +1,14 @@
+const accountID = 144553;
+let accountEmail = "ritesh@gmail.com"
+var accountPassword = "12345"
+accountCity="Jaipur"
+let accountState;
+
+//accountID=2 //not allowwed
+
+accountEmail = "hc@hc.com"
+accountPassword = "2121212121"
+
+console.log(accountID);
+
+console.table([accountID,accountEmail,accountPassword,accountState]);
---
+++ JavaScript/02_Datatypes.js
@@ -0,0 +1,12 @@
+"use strict";// treat all JS code as newer version
+
+//alert(3 + 3)//we are using nodejs,not browser
+let age=10
+
+console.log(3 + 3);
+
+console.log(typeof null);
+
+console.log(typeof age);
+
+console.log(typeof undefined);
