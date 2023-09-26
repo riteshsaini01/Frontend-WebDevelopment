7be1cb6514d2d95b346efd5944d10f843fb79c10
riteshsaini01 <b210885@skit.ac.in>
2023-09-26 23:58:18 +0530

    operators

---
+++ JavaScript/03_Operators.js
@@ -0,0 +1,44 @@
+// Arithmetic Operators
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+let remainder = num1 % num2;
+
+console.log(`Arithmetic Operators:`);
+console.log(`Sum: ${sum}`);
+console.log(`Difference: ${difference}`);
+console.log(`Product: ${product}`);
+console.log(`Quotient: ${quotient}`);
+console.log(`Remainder: ${remainder}`);
+
+// Comparison Operators
+let isEqual = num1 === num2;
+let isNotEqual = num1 !== num2;
+let isGreaterThan = num1 > num2;
+let isLessThan = num1 < num2;
+let isGreaterOrEqual = num1 >= num2;
+let isLessOrEqual = num1 <= num2;
+
+console.log(`\nComparison Operators:`);
+console.log(`Is Equal: ${isEqual}`);
+console.log(`Is Not Equal: ${isNotEqual}`);
+console.log(`Is Greater Than: ${isGreaterThan}`);
+console.log(`Is Less Than: ${isLessThan}`);
+console.log(`Is Greater or Equal: ${isGreaterOrEqual}`);
+console.log(`Is Less or Equal: ${isLessOrEqual}`);
+
+// Logical Operators
+let condition1 = true;
+let condition2 = false;
+
+let andResult = condition1 && condition2;
+let orResult = condition1 || condition2;
+let notResult = !condition1;
+
+console.log(`\nLogical Operators:`);
+console.log(`AND Result: ${andResult}`);
+console.log(`OR Result: ${orResult}`);
+console.log(`NOT Result: ${notResult}`);
